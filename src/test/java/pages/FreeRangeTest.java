9e525742a1daf1845ae681f612132322bbe44d29
Matias Vaca <matiasvaca993@gmail.com>
2025-01-18 15:42:21 -0300

    first test set up

---
+++ src/test/java/pages/FreeRangeTest.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class FreeRangeTest {
+    
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        //Initialize WebDriver for Chrome browser
+       
+
+    }
+
+}
