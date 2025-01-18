b749064ee21ea1d6cd75e9efe0bce45a91a1140c
Matias Vaca <matiasvaca993@gmail.com>
2025-01-18 16:09:21 -0300

    testng annotations

--- src/test/java/pages/FreeRangeTest.java
+++ src/test/java/pages/FreeRangeTest.java
@@ -1,7 +1,13 @@
 package pages;
 
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FreeRangeTest {
     
@@ -10,8 +16,27 @@ public class FreeRangeTest {
     @BeforeMethod
     public void setUp(){
         //Initialize WebDriver for Chrome browser
+       WebDriverManager.chromedriver().setup();
+       driver = new ChromeDriver();
        
+    }
+
+    @Test
+    public void browseToFreeRangeTesters(){
+        //Browse to the webpage
+        driver.get("https://www.freerangetesters.com");
+
 
     }
+
+    @AfterMethod
+    public void tearDown(){
+        //close the browser after test is executed
+        if(driver != null) {
+            driver.quit();
+        }
+    }
+    
+
 
 }
