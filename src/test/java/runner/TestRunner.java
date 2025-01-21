165dfbe337801b8233a834eaadcfe9104ed127eb
Matias Vaca <matiasvaca993@gmail.com>
2025-01-20 23:41:46 -0300

    testRunner changes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,8 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", //.feature files folder
-    glue = "src/test/java/steps", //package containing classes where steps are defined
+@CucumberOptions(features = "src/test/resources", //.feature files folder
+    glue = "steps", //package containing classes where steps are defined
     plugin = {"pretty","html:target/cuucumber-reports"}
     )
 
